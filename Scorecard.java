af4467e935e5b76118dfc850c55e81faa9c77a75
Anthony Finn <anthonyfinn97@icloud.com>
2018-05-21 09:23:42 -0700

    Update

--- Scorecard.java
+++ Scorecard.java
@@ -9,34 +9,6 @@ public class Scorecard {
         }
     }
     
-    public int getTotal() {
-        int total = 0;
-        for (int score : scores) { 
-            total += score; 
-        }
-        return total;
-    }
-    
-    public int getUpperTotal() {
-        int total = 0;
-        for (int i = 0; i < scores.length; i++) {
-            if (i < 6) {
-                total += scores[i];
-            }
-        }
-        return total;
-    }
-    
-    public int getLowerTotal() {
-        int total = 0;
-        for (int i = 0; i < scores.length; i++) {
-            if (i < 6) {
-                total += scores[i+6];
-            }
-        }
-        return total;
-    }
-    
     public String getPlayerName() {
         return playerName;
     }
