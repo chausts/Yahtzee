1e5ae90e8ce62601dcbe509517d56bfd2ed0e412
Anthony Finn <anthonyfinn97@icloud.com>
2018-05-23 07:42:12 -0700

    Update

--- Scorecard.java
+++ Scorecard.java
@@ -1,12 +1,57 @@
 public class Scorecard {
-    public int[] scores = new int[12];
+    public int[] scores = new int[13];
+    public boolean isComputer;
     public String playerName;
     
-    public Scorecard (String playerName) {
+    public static final int ACES = 0;
+    public static final int TWOS = 1;
+    public static final int THREES = 2;
+    public static final int FOURS = 3;
+    public static final int FIVES = 4;
+    public static final int SIXES = 5;
+    
+    public static final int THREE_OF_A_KIND = 6;
+    public static final int FOUR_OF_A_KIND = 7;
+    public static final int FULL_HOUSE = 8;
+    public static final int SMALL_STRAIGHT = 9;
+    public static final int LARGE_STRAIGHT = 10;
+    public static final int YAHTZEE = 11;
+    public static final int CHANCE = 12;
+    
+    public Scorecard (String playerName, boolean isComputer) {
         this.playerName = playerName;
+        this.isComputer = isComputer;
         for (int i = 0; i < scores.length; i++) {
             scores[i] = -1;
         }
+    }
+    
+    public void setScore(int index, int x) {
+        scores[index] = x;
+    }
+    
+    public int getScore(int index) {
+        return scores[index];
+    }
+    
+    public void setName(String playerName) {
+        this.playerName = playerName;
+    }
+    
+    public String getName() {
+        return playerName;
+    }
+    
+    public void setIsComputer(boolean isComputer) {
+        this.isComputer = isComputer;
+    }
+    
+    public boolean getIsComputer() {
+        return isComputer;
+    }
+    
+    public void addToScore(int index, int amount) {
+        scores[index] += amount;
     }
     
     public String getPlayerName() {
--- Yahtzee.java
+++ Yahtzee.java
@@ -1,5 +1,42 @@
 public class Yahtzee
 {
-    public static void main(String[] args) {
+    public static void playerTurn(Scorecard player) {
+        Roll roll = new Roll();
+    }
+    
+    public static void computerExamineTurn(Roll roll) {
+    }
+    
+    public static void computerTurn(Scorecard computer) {
+        
+    }
+    
+    public static void main(String[] humanPlayerNames, int numComputerPlayers) {
+        // Setup Players
+        int numHumanPlayers = humanPlayerNames.length;
+        int currentComputer = 1;
+        Scorecard[] players = new Scorecard[numHumanPlayers + numComputerPlayers];
+        
+        for (int i = 0; i < numHumanPlayers; i++) {
+            players[i] = new Scorecard(humanPlayerNames[i], false);
+        }
+        
+        for (int i = 0; i < numComputerPlayers; i++) {
+            players[i + numHumanPlayers] = new Scorecard("Computer" + currentComputer, true);
+            currentComputer++;
+        }
+        
+        int maxRounds = 13 * players.length;
+        int round = 0;
+        
+        while (round < maxRounds) {
+            round++;
+            Scorecard player = players[round % players.length];
+            if (player.isComputer) {
+                computerTurn(player);
+            } else {
+                playerTurn(player);
+            }
+        }
     }
 }
