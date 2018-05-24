9f249dc68573b972d75087a3aeb82d5627564a23
Anthony Finn <anthonyfinn97@icloud.com>
2018-05-24 07:30:40 -0700

    Update

--- Yahtzee.java
+++ Yahtzee.java
@@ -10,31 +10,6 @@ public class Yahtzee
         int maxRolls = 3;
         int finalMoveIndex = 0;
         int finalMoveScore = 0;
-        
-        while (numRolls < maxRolls) {
-            int maxScore = 0;
-            ArrayList<Integer> open = new ArrayList<Integer>();
-            for (int i = 0; i < computer.scores.length; i++) {
-                if (computer.scores[i] > 0) open.add(computer.scores[i]);
-            }
-            for (int i = 0; i < open.size(); i++) {
-                int score = 0;
-                int index = 0;
-                int temp = 0;
-                //UPPER
-                for (int c = 1; c < 7; c++) {
-                    for (int k = 0; k < roll.rolls.length; k++) {
-                        if (roll.rolls[k].face == c) temp++;
-                        if (temp > score) {
-                            score = temp;
-                            index = c-1;
-                            System.out.println(score + index);
-                        }
-                    }
-                }
-            }
-            break;
-        }
         computer.setScore(finalMoveIndex, finalMoveScore);
         return roll;
     }
